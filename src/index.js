bc6f0056b5317634838ee47d5fa2fd017e269d69
Yaroslav <yaroslav_dmitriev@guarantpay.com>
2022-07-23 16:18:54 +0300

    recomment strict mode

--- src/index.js
+++ src/index.js
@@ -11,9 +11,9 @@ import { App } from './App';
 const root = createRoot(document.getElementById('root'));
 
 root.render(
-    // <React.StrictMode>
+    <React.StrictMode>
         <Provider store={store}>
             <App />
         </Provider>
-    // </React.StrictMode>
+    </React.StrictMode>
 )
