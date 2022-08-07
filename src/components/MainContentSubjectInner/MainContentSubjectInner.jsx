b1e165c38f683cecb2d7152725156aef1d51cb79
Yaroslav <yaroslav_dmitriev@guarantpay.com>
2022-08-07 14:19:54 +0300

    add subjectsQuery in mainContentSubjectInner

--- src/components/MainContentSubjectInner/MainContentSubjectInner.jsx
+++ src/components/MainContentSubjectInner/MainContentSubjectInner.jsx
@@ -6,48 +6,51 @@ import {subjectInnerHook} from "../../store/subjectInner/subjectInnerHook";
 import {ErrorLine} from "../../molecules/ErrorLine/ErrorLine";
 import {MainContent} from "../../molecules/MainContent/MainContent";
 import {subjectInnerInit} from "../../store/subjectInner/subjectInnerActions";
-import {subjectsHook} from "../../store/subjects/subjectsHook";
-import {subjectsInit} from "../../store/subjects/subjectsActions";
+import {useQuery} from "@tanstack/react-query";
+import {subjectsQuery} from "../../queryes/subjects";
 
 export const MainContentSubjectInner = memo(() => {
-    const { subjectId } = useParams();
-    const { subjectInnerInited, subjectInnerLoading, subjectInnerError, subjectInner, subjectInnerEducators } = subjectInnerHook(subjectId);
-    const { subjectsInited, subjectsLoading, subjectsError, subjects } = subjectsHook();
-    const dispatch = useDispatch()
+    const {subjectId} = useParams();
+    const {
+        subjectInnerInited,
+        subjectInnerLoading,
+        subjectInnerError,
+        subjectInner,
+        subjectInnerEducators
+    } = subjectInnerHook(subjectId);
+    const all = useQuery(["subjects"], subjectsQuery);
+    const dispatch = useDispatch();
 
     useEffect(() => {
-        if (!subjectInnerInited && !subjectInnerLoading) dispatch(subjectInnerInit(subjectId));
-        if (!subjectsInited && !subjectsLoading) dispatch(subjectsInit());
-    }, [])
+        if (!subjectInnerInited && !subjectInnerLoading)
+            dispatch(subjectInnerInit(subjectId));
+    }, []);
 
     const title = subjectInnerInited ? subjectInner.title : "Предмет...";
 
     return (
-        <MainContent
-            cssClass="page__mainContainer"
-            title={title}
-        >
-            {(subjectInnerLoading || subjectsLoading) && "Loading"}
+        <MainContent cssClass="page__mainContainer" title={title}>
+            {(subjectInnerLoading || all.isLoading) && "Loading"}
 
-            {subjectsInited && subjectInnerInited &&
+            {all.isSuccess && subjectInnerInited && (
                 <SubjectInner
                     title={null}
                     educators={subjectInnerEducators}
-                    subjects={subjects}
+                    subjects={all.data.data}
                     subjectId={subjectInner.id}
-                />}
+                />
+            )}
 
-            {subjectInnerError &&
+            {subjectInnerError && (
                 <ErrorLine
                     message={subjectInnerError}
                     reload={() => dispatch(subjectInnerInit(subjectId))}
-                />}
+                />
+            )}
 
-            {subjectsError &&
-                <ErrorLine
-                    message={subjectsError}
-                    reload={() => dispatch(subjectsInit())}
-                />}
+            {all.isError && (
+                <ErrorLine message={all.error.error} reload={all.refetch} />
+            )}
         </MainContent>
-    )
-})
+    );
+});
