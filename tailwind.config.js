b85d9b67cd3b3322d2213b00f74964bd899b3ecc
Yaroslav <yaroslav_dmitriev@guarantpay.com>
2022-08-08 11:22:21 +0300

    add tailwind rounded, color, flex, flex-col

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,jsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: ["./src/**/*.{html,js,jsx}"],
+    theme: {
+        borderRadius: {
+            none: "0",
+            small: "10px",
+            medium: "20px",
+            big: "30px"
+        },
+        colors: {
+            body: "#e9eeff",
+            white: "#ffffff",
+            layoutBg: "#f9f9f9",
+            layout: "#ececec",
+            redPlaceholder: "#ffcccc",
+            red: "#ff2b2b",
+            text: "#303030",
+            link: "#0059FF"
+        },
+        spacing: {
+            small: "10px",
+            medium: "20px",
+            big: "30px"
+        },
+        extend: {}
+    },
+    plugins: []
+};
